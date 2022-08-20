6e22085d023857acc9226c99f9e24036616096b4
Lucas <90116481+LucasRibeiro19@users.noreply.github.com>
2022-08-20 11:33:43 +0200

    Fixed fields resetting and submit button state

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -5,6 +5,7 @@ import { LoggingFormsContext } from '../../context/LoggingFormsContext';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { CloseButton } from 'react-bootstrap';
+import { isDisabled } from '@testing-library/user-event/dist/utils';
 
 const style = {
     width: '350px',
@@ -14,34 +15,40 @@ const style = {
 };
 
 export default function SignUpForm() {
-
-
+    
+    
     const {signUp} = useContext(UserContext);
-
+    
     
     const {
         formState, 
         toggleForms, 
     } = useContext(LoggingFormsContext); //form Visibility
-
-
+    
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState(""); //Password control 
     const [repeatPassword, setRepeatPassword] = useState(""); //Confirm password control 
     const [signupErr, setSignupErr] = useState(""); // Form submit error manager
-
+    
     const navigate = useNavigate();
+    const closeBtnClick = () => { // Close button click actions
+        toggleForms("signIn");
+        resetFields();
+    };
 
-    const inputs = useRef([]); 
-
-    const addInputs = el => { //getting form values
-        if(el && !inputs.current.includes(el)){
-          inputs.current.push(el)
-        }
+    const handleEmail = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value);
     };
 
     const handlePassword =  (e) => { //Live strength password check
         e.preventDefault();
-        setPassword(e.target.value)
+        setPassword(e.target.value);
+    };
+    
+    const handleRepeatPassword = (e) => {
+        e.preventDefault();
+        setRepeatPassword(e.target.value);
     };
 
     const containsLowercase = /[a-z]/.test(password); //password lowercase check
@@ -51,13 +58,13 @@ export default function SignUpForm() {
     const passwordLength = password.length >= 8; //password length check
     const strongPasswordRegExp = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})'); //check critereas
     const strongPasswordTest = strongPasswordRegExp.test(password);
-
-    const handleRepeatPassword = (e) => {
-        e.preventDefault()
-        setRepeatPassword(e.target.value)
-    };
-
     const passwordsMatches = (repeatPassword !== '') && repeatPassword === password; // Password confirm match test 
+
+    const resetFields = () =>{
+        setEmail("");
+        setPassword("");
+        setRepeatPassword("");
+    };
 
     const handleSubmit = async (e) => { //Submit signup form
         e.preventDefault();
@@ -66,20 +73,20 @@ export default function SignUpForm() {
             return;
         };
 
-        if (inputs.current[1].value !==  inputs.current[2].value) {
+        if(!passwordsMatches){
             return;
-        };
+        }
 
         try{
 
             const cred = await signUp(
-                inputs.current[0].value,
-                inputs.current[1].value
-            )
+                email,
+                password
+            );
 
-            formRef.current.reset();
             console.log(cred);
-            toggleForms("signIn")
+            resetFields();
+            toggleForms("signIn");
 
         }catch(err){
             if(err.code === "auth/invalid-email"){
@@ -91,7 +98,8 @@ export default function SignUpForm() {
         }
 
     };
-    const formRef = useRef();
+
+
 return (
     <>
         {formState.signUpForm &&(
@@ -99,22 +107,21 @@ return (
             style={style}
             >
                 <CloseButton
-                onClick={() => toggleForms("signIn")}
+                onClick={closeBtnClick}
                 />
                 <h1 className='text-center'>Inscription</h1>
                 <Form
                 onSubmit={handleSubmit}
-                ref={formRef}
                 >
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control ref={addInputs} type="email" placeholder="Email" required />
+                    <Form.Control type="email" placeholder="Email" value={email} onChange={handleEmail} required />
                     <p className='text-center text-danger'>{signupErr}</p>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formCreatePassword">
                     <Form.Label>Mot de passe</Form.Label>
-                    <Form.Control className={strongPasswordTest ? 'form-control is-valid' : null} onChange={handlePassword} ref={addInputs} value={password} type="password" placeholder="Mot de passe" />
+                    <Form.Control className={strongPasswordTest ? 'form-control is-valid' : null} onChange={handlePassword} value={password} type="password" placeholder="Mot de passe" />
                 </Form.Group>
                 <ul>
                     <li className={passwordLength ? 'text-success' : null}>8 charactères minimum</li>
@@ -125,10 +132,10 @@ return (
                 </ul>
                 <Form.Group className="mb-3" controlId="formRepeatPassword">
                     <Form.Label>Confirmer mot de passe</Form.Label>
-                    <Form.Control className={passwordsMatches ? 'form-control is-valid' : 'form-control is-invalid'} onChange={handleRepeatPassword} ref={addInputs} type="password" value={repeatPassword} placeholder="Confirmer" />
+                    <Form.Control className={passwordsMatches ? 'form-control is-valid' : 'form-control is-invalid'} onChange={handleRepeatPassword} type="password" value={repeatPassword} placeholder="Confirmer" />
                 </Form.Group>
                 <div className="text-center">
-                    <Button variant="dark" type="submit">
+                    <Button className={passwordsMatches ? null : 'disabled'} variant="dark" type="submit">
                     Valider
                     </Button>
                 </div>
